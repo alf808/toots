2c57fd6e43ba68fa15ce1f97feb80c78b85962fd
alf808 <alf@akademe.net>
2019-08-13 14:50:31 -0400

    trying to get some clean data

--- twitbot.py
+++ twitbot.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+# import csv
 
-url = "https://twitter.com/search"
+url = "https://twitter.com/pattonoswalt"
+tweets = []
+results = requests.get(url)
+content = BeautifulSoup(results.text, 'html5lib')
 
-results = requests.get(url, params={'q':'cybersecurity'})
-soup = BeautifulSoup(results.text, 'html5lib')
-print(soup.prettify())
+selected = content.select('div.tweet')
+c = 0
+for tweet in selected:
+    c += 1
+    sel_p = tweet.select('p.tweet-text')[0].get_text()
+    tweets.append(f'{c}: {sel_p}')
+    # print(sel_p)
+strung = '\n'.join(tweets)
 
-# users = soup.select("div.tweet a.js-user-profile-link")
-
-usernames = soup.select("div.tweet strong.fullname")
-for un in usernames:
-    print(un.text)
+with open('temp.txt', 'w') as fo:
+    fo.write(strung)
