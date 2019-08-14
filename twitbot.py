d983e173d57adaf6fafdb6652c132986e06a72d3
alf808 <alf@akademe.net>
2019-08-14 02:05:58 -0400

    updated for hashtag

--- twitbot.py
+++ twitbot.py
@@ -2,18 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 # import csv
 
-url = "https://twitter.com/pattonoswalt"
+url = "https://twitter.com/search"
 tweets = []
-results = requests.get(url)
-content = BeautifulSoup(results.text, 'html5lib')
-
+results = requests.get(url,
+    params={'q':'%23fredo'},
+    headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel   Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.     3809.100 Safari/537.36'}
+)
+content = BeautifulSoup(results.text, 'html.parser')
+# print(content)
 selected = content.select('div.tweet')
+print(selected)
 c = 0
 for tweet in selected:
     c += 1
-    sel_p = tweet.select('p.tweet-text')[0].get_text()
-    tweets.append(f'{c}: {sel_p}')
-    # print(sel_p)
+    sel_name = tweet.select('strong.fullname')[0]
+    sel_handle = tweet.select('span.username')[0]
+    sel_pic = tweet.select('img.avatar')[0]
+    sel_p = tweet.select('p.tweet-text')[0]
+    tweets.append(f'{c}: {sel_p}, {sel_handle}, {sel_name}, {sel_pic}')
+#     print(tweet)
+
 strung = '\n'.join(tweets)
 
 with open('temp.txt', 'w') as fo:
